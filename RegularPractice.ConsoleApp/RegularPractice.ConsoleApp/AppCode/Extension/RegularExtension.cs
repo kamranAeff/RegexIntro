0fd0f3cd4aae43c197e5234992cb1e0b5aff0e34
kamranAeff <kamran_aeff@mail.ru>
2021-05-13 09:37:26 +0400

    Add Docs and Solution

---
+++ RegularPractice.ConsoleApp/RegularPractice.ConsoleApp/AppCode/Extension/RegularExtension.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularPractice.ConsoleApp.AppCode.Extension
+{
+    static partial class Extension
+    {
+        public const string EMAIL_PATTERN = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+        public const string PHONE_PATTERN = @"^(\+994|0)(50|51|55|70|77|99|0)-(\d{3}-\d{2}-\d{2})$";
+        public const string NAME_PATTERN = @"^[A-ZƏ]{1}[a-zə]{2,}$";
+        public const string PASSWORD_PATTERN = @"^((?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#])).{8,}$";
+
+
+        public static bool IsEmail(this string value)
+        {
+            Match emailResult = Regex.Match(value, EMAIL_PATTERN);
+
+            if (emailResult.Success)
+                return true;
+
+            return false;
+        }
+
+        public static bool IsPhone(this string value)
+        {
+            if (Regex.IsMatch(value, PHONE_PATTERN))
+                return true;
+
+            return false;
+        }
+
+
+
+        public static bool IsName(this string value)
+        {
+            if (Regex.IsMatch(value, NAME_PATTERN))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsPassword(this string value)
+        {
+            if (Regex.IsMatch(value, PASSWORD_PATTERN))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ RegularPractice.ConsoleApp/RegularPractice.ConsoleApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using RegularPractice.ConsoleApp.AppCode.Extension;
+using System;
+using System.Text.RegularExpressions;
+
+namespace RegularPractice.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Şifrə: ");
+        l2:
+            string pwd = Console.ReadLine();
+
+            if (!pwd.IsPassword())
+            {
+                Console.Write("Düzgün formatda deyil,yenidən daxil edin:");
+                goto l2;
+            }
+
+            Console.WriteLine($"Sizin şifrəniz, {pwd}");
+
+            return;
+
+            Console.Write("Adınız: ");
+            l1: 
+            string name = Console.ReadLine();
+
+            if (!name.IsName())
+            {
+                Console.Write("Düzgün formatda deyil,yenidən daxil edin:");
+                goto l1;
+            }
+
+            Console.WriteLine($"Salam, {name}");
+
+            return;
+            string phone = "555-455-12-13";
+
+            if (phone.IsPhone())
+            {
+                Console.WriteLine($"{phone} uygun formatdadir");
+            }
+            else
+            {
+                Console.WriteLine($"{phone} uygun formatda deyil");
+            }
+
+            phone = "055-455-12-13";
+
+            if (phone.IsPhone())
+            {
+                Console.WriteLine($"{phone} uygun formatdadir");
+            }
+            else
+            {
+                Console.WriteLine($"{phone} uygun formatda deyil");
+            }
+
+            phone = "+99455-455-12-13";
+
+            if (phone.IsPhone())
+            {
+                Console.WriteLine($"{phone} uygun formatdadir");
+            }
+            else
+            {
+                Console.WriteLine($"{phone} uygun formatda deyil");
+            }
+
+
+            string email = "555-455-1-13";
+            Console.WriteLine("##########################");
+            email = "test";
+            bool state = email.IsEmail();
+
+            Console.WriteLine(state);
+
+            email = "test@mail.ru";
+            state = Extension.IsEmail(email);
+            Console.WriteLine(state);
+
+            email = "test@mail";
+            state = email.IsEmail();
+            Console.WriteLine(state);
+        }
+
+       
+    }
+}
